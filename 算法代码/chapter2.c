142c064c078fe9b40ed8caf2f22d108098e5375c
Asuka02 <72290940+use1es@users.noreply.github.com>
2021-08-09 18:51:38 +0800

    Update chapter2.c

--- 算法代码/chapter2.c
+++ 算法代码/chapter2.c
@@ -12,4 +12,17 @@ void Delete_x(sqList_p L,int x){
     }
     L->length = L->length - k;
 }
-
+//删除有序表中重复的元素
+//算法思想：设置一个临时变量等于表内的第一个元素，在扫描表的时候让表中元素和临时变量比较是否相同，若不同则添入表中且让临时变量等于新元素。
+void delete_rep(sqList_p L){
+    int tmp = L->list[0];
+    int k = 1;
+    for (int i = 0; i < L->length;i++){
+        if(L->list[i]!=tmp){
+            L->list[k] = L->list[i];
+            tmp = L->list[i];
+            k++;
+        }
+    }
+    L->length = k;
+}
